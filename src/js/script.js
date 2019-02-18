ff41f66c93feb8955079500a2ded1f942693415b
Natalka <natalka.zakalyk@gmail.com>
2019-02-18 21:02:01 +0200

    Первая домашка

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,22 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     const cartWrapper = document.querySelector('.cart__wrapper'),
-    cart = document.querySelector('.cart'),
-    close = document.querySelector('.cart__close'),
-    open = document.querySelector('#cart'),
-    goodsBtn = document.querySelectorAll('.goods__btn'),
-    products = document.querySelectorAll('.goods__item'),
-    confirm = document.querySelector('.confirm'),
-    badge = document.querySelector('.nav__badge'),
-    totalCost = document.querySelector('.cart__total > span'),
-    titles = document.querySelector('.goods__title');
+        cart = document.querySelector('.cart'),
+        close = document.querySelector('.cart__close'),
+        open = document.querySelector('#cart'),
+        goodsBtn = document.querySelectorAll('.goods__btn'),
+        products = document.querySelectorAll('.goods__item'),
+        confirm = document.querySelector('.confirm'),
+        badge = document.querySelector('.nav__badge'),
+        totalCost = document.querySelector('.cart__total > span'),
+        titles = document.querySelector('.goods__title');
 
-    function openCart(){
+    function openCart() {
         cart.style.display = 'block';
         document.body.style.overflow = 'hidden';
     }
 
-    function closeCart(){
+    function closeCart() {
         cart.style.display = 'none';
         document.body.style.overflow = '';
     }
@@ -24,12 +24,12 @@ window.addEventListener('DOMContentLoaded', () => {
     open.addEventListener('click', openCart);
     close.addEventListener('click', closeCart);
 
-    goodsBtn.forEach(function(btn,i){
+    goodsBtn.forEach(function (btn, i) {
         btn.addEventListener('click', () => {
             let item = products[i].cloneNode(true),
-            trigger = item.querySelector('button'),
-            removeBtn = document.createElement('div'),
-            empty = cartWrapper.querySelector('.empty');
+                trigger = item.querySelector('button'),
+                removeBtn = document.createElement('div'),
+                empty = cartWrapper.querySelector('.empty');
 
             trigger.remove();
 
@@ -39,7 +39,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
             cartWrapper.appendChild(item);
 
-            if(empty){
+            if (empty) {
                 empty.remove();
             }
         });
